c0ee2ed8856c3169db96a16cb50fd2461333308a
20040045 <20040045@studenti.uniupo.it>
2024-12-30 21:00:20 +0100

    Delete parcheggio directory

--- parcheggio/parcheggio/parcheggio/parchggio/src/main/java/Principale/Parcheggio/Repository/ParkingSpotRepository.java
+++
@@ -1,8 +0,0 @@
-package Principale.Parcheggio.Repository;
-
-import Principale.Parcheggio.Models.ParkingSpot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Integer> {
-    // Puoi aggiungere query personalizzate se necessario
-}
