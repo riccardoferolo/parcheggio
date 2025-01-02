8cdfffe227771430c57856b270d1f0ace1e709d7
20040045 <20040045@studenti.uniupo.it>
2025-01-02 14:44:42 +0100

    Add files via upload

---
+++ parcheggio/src/main/java/Principale/Parcheggio/Controllers/PaymentController.java
@@ -0,0 +1,79 @@
+package Principale.Parcheggio.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import Principale.Parcheggio.Models.*;
+import Principale.Parcheggio.Services.*;
+import org.springframework.http.HttpStatus;
+
+
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @Autowired
+    private ChargeRequestService chargeRequestService;  // Servizio per recuperare ChargeRequest
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createPayment(@RequestBody Long chargeRequestId) {
+        try {
+            // Recupera la ChargeRequest usando il suo ID
+            Optional<ChargeRequest> chargeRequestOpt = chargeRequestService.getChargeRequestById(chargeRequestId);
+
+            // Verifica se la ChargeRequest esiste
+            if (!chargeRequestOpt.isPresent()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ChargeRequest con ID " + chargeRequestId + " non trovata.");
+            }
+
+            ChargeRequest chargeRequest = chargeRequestOpt.get();
+
+            // Crea il pagamento basato su questa ChargeRequest
+            Payment payment = paymentService.createPayment(chargeRequest);
+
+            return ResponseEntity.ok(payment);
+        } catch (Exception e) {
+            // Gestione delle eccezioni e log
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore durante la creazione del pagamento: " + e.getMessage());
+        }
+    }
+
+
+    // Recupera un pagamento tramite ID
+    @GetMapping("/recuper_id")
+    public ResponseEntity<Payment> getPaymentById(@RequestBody Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        return ResponseEntity.ok(payment);
+    }
+
+    // Endpoint per eliminare tutti gli utenti
+    @DeleteMapping("/delete-all")
+    public ResponseEntity<String> eliminaTuttiPagamenti() {
+        try {
+            paymentService.eliminaTuttiPagamenti();
+            return ResponseEntity.ok("tutti i pagamenti sono stati eliminati");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Errore durante l'eliminazione dei pagamenti: " + e.getMessage());
+        }
+    }
+
+    // Endpoint per cancellare tutti i pagamenti associati a un ID utente
+    @DeleteMapping("/delete/user/id")
+    public ResponseEntity<String> deleteRequestsByUserId(@RequestBody Long userId) {
+        try {
+            paymentService.eliminaPagamentoperUserId(userId);
+            return ResponseEntity.ok("Tutti i pagamenti  per l'utente con ID " + userId + " sono stati cancellati.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore durante la cancellazione delle richieste: " + e.getMessage());
+        }
+    }
+
+}
+
---
+++ parcheggio/src/main/java/Principale/Parcheggio/Models/ChargeRequest.java
@@ -0,0 +1,123 @@
+package Principale.Parcheggio.Models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
+
+import java.sql.Time;
+import java.time.LocalDate;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+public class ChargeRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // Aggiunta di un campo ID per l'entità
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate Giorno;
+    private Integer Percentuale_iniziale;
+    private Integer Percentuale_richiesta;
+    private Time durata;
+
+    private Time Ora;
+
+    private Time OraFine;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // Relazione con l'entità User
+
+    @Column(name = "pagare", nullable = false, columnDefinition = "FLOAT DEFAULT 0")
+    private double Pagare;
+
+    private String Targa;
+
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Time getdurata() {
+        return durata;
+    }
+
+    public void setdurata(Time durata) {
+        this.durata = durata;
+    }
+
+    public Integer getpercentuale_iniziale() {
+        return Percentuale_iniziale;
+    }
+
+    public void setPercentuale_iniziale(Integer percentuale) {
+        this.Percentuale_iniziale = percentuale;
+    }
+
+    public Integer getPercentuale_richiesta() {
+        return Percentuale_richiesta;
+    }
+
+    public void setPercentuale_richiesta(Integer percentuale) {
+        this.Percentuale_richiesta = percentuale;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getGiorno() {
+        return Giorno;
+    }
+
+    public void setGiorno(LocalDate giorno) {
+        this.Giorno = giorno;
+    }
+
+    public Time getOra() {
+        return Ora;
+    }
+
+    public void setOra(Time ora) {
+        this.Ora = ora;
+    }
+
+    public double getPagare() {
+        return Pagare;
+    }
+
+    public void setPagare(double pagare) {
+        this.Pagare = pagare;
+    }
+
+    public Time getOraFine() {
+        return OraFine;
+    }
+
+    public void setOraFine(Time oraFine) {
+        OraFine = oraFine;
+    }
+
+    public String getTarga() {
+        return Targa;
+    }
+
+    public void setTarga(String targa) {
+        Targa = targa;
+    }
+
+}
+
+
